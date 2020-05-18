3fb4c3a7d97d9e47db5843cf78fce18d08e6ec9d
Mahesh <mahesh@retailon.in>
2020-05-18 09:24:57 +0530

    for new setup

---
+++ app/design/frontend/Practical/test/Magento_Newsletter/web/js/custom.js
@@ -0,0 +1,9 @@
+ require(['jquery'], function($){ 
+    $(document).ready(function(){
+	  $(".newsletter .subscribe").click(function(){
+	  	setTimeout(function() {
+	    	alert("This is just a test");
+	    }, 5000);
+	  });
+	});
+ });
